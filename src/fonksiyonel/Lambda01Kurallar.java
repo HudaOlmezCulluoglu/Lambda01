b9d42716a067e762eab88d8b35429c592b8ab1cb
HudaOlmezCulluoglu <culluhulya9@gmail.com>
2021-04-18 17:29:10 +0300

    hele Son

--- src/fonksiyonel/Lambda01Kurallar.java
+++ src/fonksiyonel/Lambda01Kurallar.java
@@ -4,6 +4,7 @@ public class Lambda01Kurallar {
     public static void main(String[] args) {
          System.out.println("Hasgeldin Ramazan  02");
         System.out.println("Hasgeldin Ramaza 03n");
-
+        System.out.println("Hasgeldin Ramazan");
+        System.out.println("Hasgeldin Ramazan");
     }
 }
