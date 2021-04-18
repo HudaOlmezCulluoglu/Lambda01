2f3b6e7389d85049c4d479d56943d4073e3d6640
HudaOlmezCulluoglu <81856087+HudaOlmezCulluoglu@users.noreply.github.com>
2021-04-18 17:27:45 +0300

    Update Lambda01Kurallar.java

--- src/fonksiyonel/Lambda01Kurallar.java
+++ src/fonksiyonel/Lambda01Kurallar.java
@@ -3,6 +3,7 @@ package fonksiyonel;
 public class Lambda01Kurallar {
     public static void main(String[] args) {
          System.out.println("Hasgeldin Ramazan  02");
+        System.out.println("Hasgeldin Ramaza 03n");
 
     }
 }
